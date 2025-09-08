a89ac1dc75aacb52de127c3cdeef3c79d02fe11c
Jibran Hussain Mir <mirjibranhussain@gmail.com>
2025-09-08 23:50:06 +0530

    feat: Integrate textsplitters dependency and update chat service to utilize helper functions

--- api/v1/services/chat.js
+++ api/v1/services/chat.js
@@ -1,8 +1,18 @@
+import helper from '../../../utils/helper.js';
+
 async function answerUsersQuestion(req, res) {
     try {
-        const documentId = req.body.documentId;
-        const context = req.body.context;
-        const question = req.body.question;
+        // const documentId = req.body.documentId;
+        // const context = req.body.context;
+        // const question = req.body.question;
+
+        const documentPath = './documents/historyOfKashmir.txt'
+        const response = await helper.splitTextIntoChunks('recursiveCharacter', { 
+            filePath: documentPath,
+            chunkSize: 1000,
+            chunkOverlap: 200
+         });
+         console.log('Response from splitTextIntoChunks', response)
         
     } catch(error) {
         
